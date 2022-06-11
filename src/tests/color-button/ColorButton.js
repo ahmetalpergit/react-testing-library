ff0da6a9d85592d74f8a720bd5a2c71e766768e3
ahmetalpergit <ahmetalperwork@gmail.com>
2022-06-11 22:27:22 +0200

    button click functionality testing

--- src/tests/color-button/ColorButton.js
+++ src/tests/color-button/ColorButton.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ColorButton = () => {
-  return <button style={{ backgroundColor: 'red' }}>Change to blue</button>;
+  const [buttonSettings, setButtonSettings] = useState({
+    color: 'red',
+    text: 'Change to blue',
+  });
+
+  const buttonClickHandler = () => {
+    buttonSettings.color === 'red'
+      ? setButtonSettings({
+          color: 'blue',
+          text: 'Change to red',
+        })
+      : setButtonSettings({
+          color: 'red',
+          text: 'Change to blue',
+        });
+  };
+
+  return (
+    <button
+      style={{ backgroundColor: buttonSettings.color }}
+      onClick={buttonClickHandler}
+    >
+      {buttonSettings.text}
+    </button>
+  );
 };
 
 export default ColorButton;
--- src/tests/color-button/colorButton.test.js
+++ src/tests/color-button/colorButton.test.js
@@ -1,14 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import ColorButton from './ColorButton';
 
-test('button has correct initial color', () => {
+test('Change to blue button exists', () => {
+  render(<ColorButton />);
+
   //find an element with a role of button and text change to blue
+  const colorBtn = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorBtn).toBeInTheDocument();
+});
+
+test('button has correct initial color', () => {
   render(<ColorButton />);
   const colorBtn = screen.getByRole('button', { name: 'Change to blue' });
+
   //expect the background color to  be red
   expect(colorBtn).toHaveStyle({ backgroundColor: 'red' });
 });
 
-test('button has correct initial text', () => {});
+test('button turns blue when clicked', () => {
+  render(<ColorButton />);
 
-test('button turns blue when clicked', () => {});
+  //get the button element
+  const colorBtn = screen.getByRole('button', { name: 'Change to blue' });
+
+  //click the button
+  fireEvent.click(colorBtn);
+
+  //expect the button color to change to blue
+  expect(colorBtn).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+test('button text changes to Change to red', () => {
+  render(<ColorButton />);
+  const colorBtn = screen.getByRole('button', { name: 'Change to blue' });
+
+  fireEvent.click(colorBtn);
+
+  //check if text changed to Change to red
+  expect(colorBtn).toHaveTextContent('Change to red');
+});
